5782ad1b33cdb2cb8be57f452af1697767846064
jordanpetersen <jordan.petersen@fanduel.com>
2020-02-05 21:13:47 -0600

    build in pipeline

--- app/src/main/java/com/example/pipeline/MainActivity.kt
+++ app/src/main/java/com/example/pipeline/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // random comment for PR
         setContentView(R.layout.activity_main)
     }
 }
